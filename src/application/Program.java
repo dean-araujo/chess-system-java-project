200068b6941205c4f367a692b9144894a84a39a9
Dean Araujo <deantarik4@gmail.com>
2023-09-07 19:48:00 -0300

    First Class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Position p = new Position(3, 5);
+		System.out.println(p);
+		
 	}
 
 }
